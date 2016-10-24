7d3222e16aa4e3cc12384dcd5378919843e5ab19
matteo brancaleoni <mbrancaleoni@voismart.it>
2016-10-24 15:33:23 +0200

    remove debug logs

--- idle.js
+++ idle.js
@@ -1,9 +1,6 @@
 var port = chrome.runtime.connect({name: "idlestatus"});
 
-console.log("got a port");
-
 port.postMessage({rq: "status"});
 port.onMessage.addListener(function(msg) {
     window.postMessage({ type: "idlestatus", state: msg }, "*");
-    console.log(msg);
 });
